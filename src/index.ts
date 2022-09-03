e243ad64235eb939b8b36b9d18d1cdac45ace0da
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-09-03 12:54:20 +0200

    Hello world Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello from typescript");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
