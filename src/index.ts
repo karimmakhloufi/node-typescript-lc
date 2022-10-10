2976feb5f0eab353e90ba87602866596b85f4e77
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-10-10 14:53:02 +0200

    First step moving from express to apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,42 @@
 import "reflect-metadata";
-import express from "express";
-import cors from "cors";
+import { ApolloServer, gql } from "apollo-server";
 import dataSource from "./utils";
-import wilderController from "./controller/wilder";
-import skillController from "./controller/skill";
-import gradeController from "./controller/grade";
+import { Wilder } from "./entity/wilder";
 
-const app = express();
+const typeDefs = gql`
+  type Wilder {
+    name: String
+  }
 
-app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+  type Query {
+    getAllWilders: [Wilder]
+  }
+`;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.get("/api/wilder", wilderController.read);
-app.post("/api/wilder", wilderController.create);
-
-app.get("/api/skill", skillController.read);
-app.post("/api/skill", skillController.create);
-
-app.post("/api/grade", gradeController.create);
+const resolvers = {
+  Query: {
+    getAllWilders: async () =>
+      await dataSource.manager.find(Wilder, {
+        relations: {
+          grades: {
+            skill: true,
+          },
+        },
+      }),
+  },
+};
 
 const port = 5000;
 
 const start = async (): Promise<void> => {
   await dataSource.initialize();
-  app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
   });
+
+  const { url } = await server.listen({ port });
+  console.log(`🚀  Server ready at ${url}`);
 };
 
 void start();
